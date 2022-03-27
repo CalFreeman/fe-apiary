08b33d027ffec54419ec42cf1c861a10d52b68b4
calfreeman <ctfreeman1s@gmail.com>
2022-03-27 17:41:00 -0400

    working on post req

--- pages/farms.js
+++ pages/farms.js
@@ -14,17 +14,20 @@ const NewFarm = ({ apiary = [] }) => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		//console.log(farm)
 
-		const res = await fetch("/farm", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ farm: farm }),
-		});
-		if (res.ok) {
-			setSaved(true);
-		} else {
-			setError(`Error ${res.status} :: ${res.statusText}`);
-		}
+		//const res = await fetch("/api/farm", {
+		//	method: "POST",
+		//	headers: { "Content-Type": "application/json" },
+		//	body: JSON.stringify({ farm: farm }),
+		//});
+
+		FarmService.create(farm)
+		//if (res.ok) {
+		//	setSaved(true);
+		//} else {
+		//	setError(`Error ${res.status} :: ${res.statusText}`);
+		//}
 	};
 
 	const handleChange = (e) => {
@@ -37,21 +40,16 @@ const NewFarm = ({ apiary = [] }) => {
 		}));
 	};
 
-	//const categoryOptions = categories.map((c) => (
-	//	<option key={c.id} value={c.id}>
-	//		{c.name}
-	//	</option>
-	//));
-
 	return (
 		<div>
-			<h1>Create New farm</h1>
+			<h1>All farms</h1>
 			<Link href="/farmss">
 				<button>
-					<a>All farm</a>
+					<a>All farms</a>
 				</button>
 			</Link>
 
+			<h1>New farm</h1>
 			{error && <h3>{error}</h3>}
 			{saved && <h3>Congrats! The farm '{farm.name}' was saved successfully.</h3>}
 
